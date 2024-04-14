b2252979c7a6e7458eb6482c9cae20ff7769c08c
root <root@Matheo>
2024-04-14 18:40:41 +0200

    Quartz sync: Apr 14, 2024, 6:40 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Blog de Matchag ",
+    pageTitle: "Blog de Matchag",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
